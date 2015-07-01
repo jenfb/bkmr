c41eafa4a037eae050bdd735e3164e7c5699619f
Jennifer B <jenniferfederbobb@gmail.com>
2015-07-01 16:18:08 -0700

    Update TracePlot function

--- R/TracePlot.R
+++ R/TracePlot.R
@@ -1,11 +1,11 @@
 #' Trace plot
 #'
 #' @export
-TracePlot <- function(fit, par, comp = 1, main = "", ...) {
+TracePlot <- function(fit, par, comp = 1, sel = NULL, main = "", xlab = "iteration", ylab = "parameter value", ...) {
     if(inherits(fit, "stanfit")) {
         warning("you can use rstan::traceplot() instead")
     }
-    samps <- ExtractSamps(fit)[[par]]
+    samps <- ExtractSamps(fit, sel = sel)[[par]]
     if (!is.null(ncol(samps))) {
         nm <- colnames(samps)[comp]
         samps <- samps[, comp]
@@ -13,6 +13,6 @@ TracePlot <- function(fit, par, comp = 1, main = "", ...) {
         nm <- par
     }
     main <- paste0(main, "\n(", nm, " = ", format(mean(samps), digits = 2), ")")
-    plot(samps, type = "l", ylab = nm, main = main, ...)
+    plot(samps, type = "l", main = main, xlab = xlab,  ylab = ylab, ...)
     abline(h = mean(samps), col = "blue", lwd = 2)
 }
