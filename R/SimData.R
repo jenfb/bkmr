1f43301c7283ecdc4e28da16e98be835f26f696a
Jennifer Bobb <bobb.j@ghc.org>
2015-08-07 12:49:36 -0700

    Update file with functions simulating data.

--- R/SimData.R
+++ R/SimData.R
@@ -1,54 +1,42 @@
-GenCovarDefault <- function(z) {
-    3*cos(z[1]) + 2*rnorm(1)
-}
-
-GenExposDefault <- function(n, M) {
-    matrix(rnorm(n * M), n, M,
-           dimnames = list(NULL, paste0("expos", 1:M)))
-}
-
-HFun1 <- function(z, ind = 1) 4*plogis(z[ind], 0, 0.3)
-
-HFun2 <- function(z, ind1 = 1, ind2 = 2) 1/4*(z[ind1] + z[ind2] + 1/2*z[ind1]*z[ind2])
-
-HFun3 <- function(z, ind1 = 1, ind2 = 2) 4*plogis(1/4*(z[ind1] + z[ind2] + 1/2*z[ind1]*z[ind2]), 0, 0.3)
-
 #' Simulate dataset
 #'
-#' Simulate dataset
+#' Simulate exposure, covariate, and outcome data
 #'
 #' @export
 #'
-#' @param GenExpos A function that takes as input a vector of length \code{M} Defaults to \code{\link{GenExposDefault}}.
-#' @param GenCovar A function that takes as input a vector of length \code{M}. Defaults to \code{\link{GenCovarDefault}}.
 #' @param n Number of observations
 #' @param M Number of exposure variables to generate
 #' @param sigsq.true Variance of normally distributed residual error
 #' @param beta.true Coefficient on the covariate
-#' @param HFun A function that takes as input a vector of length \code{M} and outputs a scalar
+#' @param hfun An integer from 1 to 3 identifying which exposure response function to generate
 #' @examples
 #' set.seed(5)
 #' dat <- SimData()
+#' @details 
+#' \itemize{
+#'  \item{"hfun = 1"}{A nonlinear function of the first exposure variable}
+#'  \item{"hfun = 2"}{A linear function of the first two exposure variables and their product term}
+#'  \item{"hfun = 3"}{A nonlinear and nonadditive function of the first two exposure variables}
+#' }
 SimData <- function(n = 100, M = 5, sigsq.true = 0.5,
-                     beta.true = 2, GenCovar = NULL,
-                     GenExpos = NULL, HFun = NULL) {
-    if (is.null(GenCovar)) {
-        GenCovar <- GenCovarDefault
-    }
-    if (is.null(GenExpos)) {
-        GenExpos <- GenExposDefault
-    }
-    if (is.null(HFun)) {
-        HFun <- HFun1
-    }
-    expos <- GenExpos(n = n, M = M)
-    eps <- rnorm(n, sd = sqrt(sigsq.true))
-    h <- apply(expos, 1, HFun)
-    covar <- cbind(apply(expos, 1, GenCovar))
-    if (length(beta.true) == 1) {
-        beta.true <- rep(beta.true, ncol(covar))
-    }
-    y <- drop(covar %*% beta.true + h + eps)
-
-    dat <- list(n = n, M = M, sigsq.true = sigsq.true, beta.true = beta.true, expos = expos, h = h, covar = covar, y = y)
+                     beta.true = 2, hfun = 3) {
+  
+  if (hfun == 1) {
+    HFun <- function(z, ind = 1) 4*plogis(z[ind], 0, 0.3)
+  } else if (hfun == 2) {
+    HFun <- function(z, ind1 = 1, ind2 = 2) 1/4*(z[ind1] + z[ind2] + 1/2*z[ind1]*z[ind2])
+  } else if (hfun == 3) {
+    HFun <- function(z, ind1 = 1, ind2 = 2) 4*plogis(1/4*(z[ind1] + z[ind2] + 1/2*z[ind1]*z[ind2]), 0, 0.3)
+  } else {
+    stop("hfun must be an integer from 1 to 3")
+  }
+  
+  expos <- matrix(rnorm(n * M), n, M,
+                  dimnames = list(NULL, paste0("expos", 1:M)))
+  covar <- cbind(3*cos(expos[, 1]) + 2*rnorm(n))
+  eps <- rnorm(n, sd = sqrt(sigsq.true))
+  h <- apply(expos, 1, HFun)
+  y <- drop(covar * beta.true + h + eps)
+  
+  dat <- list(n = n, M = M, sigsq.true = sigsq.true, beta.true = beta.true, expos = expos, h = h, covar = covar, y = y, hfun = hfun, HFun = HFun)
 }
