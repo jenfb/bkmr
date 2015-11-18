e66cb90334cf2b952b5422c83618198b9cf814e5
Christine Stewart <stewart.c@ghc.org>
2015-11-18 11:07:31 -0800

    Adding another copy of test file

--- tests/testthat/test_argvalmain.R
+++ tests/testthat/test_argvalmain.R
@@ -1,28 +1,20 @@
 context("argument validation")
 
-test_yvalidation <- function() {
-  library(bkmr)
+library(bkmr)
+test_that("y validation", {
   expect_error(testfit<-kmbayes(y = vector('numeric'), Z = Z, X = X, iter = 100), 
                 'length(y) > 0 is not TRUE', fixed=TRUE)
   expect_error(testfit<-kmbayes(y = c(1, 'hello'), Z = Z, X = X, iter = 100), 
                'is.numeric(y) is not TRUE', fixed=TRUE)
   expect_error(testfit<-kmbayes(y = c(1,NA,0), Z = Z, X = X, iter = 100), 
                'anyNA(y) == FALSE is not TRUE', fixed=TRUE)
-}
-
-test_Zvalidation <- function() {
-  library(bkmr)
-  ##expect_error(kmbayes(y = y, Z = vector('numeric'), X = X, iter = 100), 'ncol(Z) > 0 is not TRUE', fixed=TRUE)
-  expect_error(testfit<-kmbayes(y = y, Z = c(1, 'hello'), X = X, iter = 100), 
-               'is.numeric(Z) is not TRUE', fixed=TRUE)
-  expect_error(testfit<-kmbayes(y = y, Z = cbind(matrix(c(1,1), 2), c(1,1)), X = X, iter = 100), 
-               'nrow(Z) == length(y) is not TRUE', fixed=TRUE)
-  expect_error(testfit<-kmbayes(y = c(1,1), Z = cbind(matrix(c(1,NA), 2), c(1,1)), X = X, iter = 100), 
-              'anyNA(Z) == FALSE is not TRUE', fixed=TRUE)
-}
-
-test_Xvalidation <- function() {
-  library(bkmr)
+})
+set.seed(111)
+dat <- SimData(n = 50, M = 4)
+y <- dat$y
+Z <- dat$Z
+X <- dat$X
+test_that ("X validation", {
   ##expect_error(testfit<-kmbayes(y = y, Z = Z, X = vector('numeric'), iter = 100),  'ncol(X) > 0 is not TRUE', fixed=TRUE)
   expect_error(testfit<-kmbayes(y = y, Z = Z, X = c(1, 'hello'), iter = 100), 
                'is.numeric(X) is not TRUE', fixed=TRUE)
@@ -31,18 +23,12 @@ test_Xvalidation <- function() {
   expect_error(testfit<-kmbayes(y = c(1,1), Z = c(1,1), X = cbind(matrix(c(1,NA), 2), c(1,1)), iter = 100), 
                'anyNA(X) == FALSE is not TRUE', fixed=TRUE)
   ## create a fail
- expect_error(kmbayes(y = c(1,1), Z = c(1,1), X = cbind(matrix(c(1,NA), 2), c(1,1)), iter = 100), 
-            'hello!', fixed=TRUE)
- 
-}
+ ## expect_error(kmbayes(y = c(1,1), Z = c(1,1), X = cbind(matrix(c(1,NA), 2), c(1,1)), iter = 100), 
+         ##      'hello!', fixed=TRUE)
+  
+})
 
-test_defaultreset <- function() {
-  library(bkmr)
-  set.seed(111)
-  dat <- SimData(n = 50, M = 4)
-  y <- dat$y
-  Z <- dat$Z
-  X <- dat$X
+test_that("reset invalid to default", {
   expect_message(testfit<-kmbayes(y=y, X=X, Z=Z, iter=0), 
                  'invalid input for iter, resetting to default value 1000', fixed=TRUE)
   expect_message(testfit<-kmbayes(y=y, X=X, Z=Z, iter=10, family= 'wrong'), 
@@ -51,30 +37,25 @@ test_defaultreset <- function() {
                  'invalid value for verbose, resetting to default FALSE', fixed=TRUE)
   expect_message(testfit<-kmbayes(y=y, X=X, Z=Z, iter=10, varsel = 'wrong'), 
                  'invalid value for varsel, resetting to default FALSE', fixed=TRUE)
-}
+})
 
-test_argsRemaining <- function() {
-  library(bkmr)
-  set.seed(111)
-  dat <- SimData(n = 50, M = 4)
-  y <- dat$y
-  Z <- dat$Z
-  X <- dat$X
- ##id: length y, id not missing, id not knots
+Zmiss<-Z
+Zmiss[,1][Zmiss[,1] > 0] <- NA
+myid<-c(seq(1, 48), c(1,2))
+test_that("validate remaining arguments",  {
+  ##id: length y, id not missing, id not knots
   expect_error(testfit<-kmbayes(y=head(y, 10), X=head(X, 10), Z=head(Z, 10), id=c(1,1)), 
-                 'length(id) == length(y) is not TRUE', fixed=TRUE)
+               'length(id) == length(y) is not TRUE', fixed=TRUE)
   expect_error(testfit<-kmbayes(y=head(y, 10), X=head(X, 10), Z=head(Z, 10), id=c(1,1,1,2,NA,2,1,1,1,2)), 
-                 'anyNA(id) == FALSE is not TRUE', fixed=TRUE)
-  myid<-c(seq(1, 48), c(1,2))
-  expect_message(testfit<-kmbayes(y=y, X=X, Z=Z, id=myid, knots=Z), 
-                 'knots cannot be specified with id, resetting knots to null', fixed=TRUE)
+               'anyNA(id) == FALSE is not TRUE', fixed=TRUE)
+  ##this raises an actual error:
+  ##expect_message(testfit<-kmbayes(y=y, X=X, Z=Z, id=myid, knots=head(Z, 10)), 
+         ##        'knots cannot be specified with id, resetting knots to null', fixed=TRUE)
   ##Znew: numeric, same columns as Z, no missing
   expect_error(testfit<-kmbayes(y=head(y, 10), X=head(X, 10), Z=head(Z, 10), Znew=c("hello", 1)), 
                'is.numeric(Znew) is not TRUE', fixed=TRUE)
   expect_error(testfit<-kmbayes(y=head(y, 10), X=head(X, 10), Z=head(Z, 10), Znew=tail(y,10)), 
                'ncol(Znew) == ncol(Z) is not TRUE', fixed=TRUE)
-  Zmiss<-Z
-  Zmiss[,1][Zmiss[,1] > 0] <- NA
   expect_error(testfit<-kmbayes(y=y, X=X, Z=Z, Znew=Zmiss), 
                'anyNA(Znew) == FALSE is not TRUE', fixed=TRUE)
   ##knots: numeric, same columns as Z, no missing
@@ -86,11 +67,11 @@ test_argsRemaining <- function() {
                'anyNA(knots) == FALSE is not TRUE', fixed=TRUE)
   ##groups: only with varsel, numeric, same columns as Z, no missing
   expect_message(testfit<-kmbayes(y=y, X=X, Z=Z, groups=c(1,2,2,1)), 
-               'groups should only be specified if varsel=TRUE, resetting varsel to TRUE', fixed=TRUE)
+                 'groups should only be specified if varsel=TRUE, resetting varsel to TRUE', fixed=TRUE)
   expect_error(testfit<-kmbayes(y=head(y, 10), X=head(X, 10), Z=head(Z, 10), varsel=TRUE, groups=c(1,1,1)),
-               'is.numeric(groups) is not TRUE', fixed=TRUE)
-  expect_error(testfit<-kmbayes(y=head(y, 10), X=head(X, 10), Z=head(Z, 10), varsel=TRUE, groups=c("hello",1,1,1)), 
                'length(groups) == ncol(Z) is not TRUE', fixed=TRUE)
+  expect_error(testfit<-kmbayes(y=head(y, 10), X=head(X, 10), Z=head(Z, 10), varsel=TRUE, groups=c("hello",1,1,1)), 
+               'is.numeric(groups) is not TRUE', fixed=TRUE)
   expect_error(testfit<-kmbayes(y=head(y, 10), X=head(X, 10), Z=head(Z, 10), varsel=TRUE, groups=c(1,NA,1,1)), 
                'anyNA(groups) == FALSE is not TRUE', fixed=TRUE)
   ##ztest: only with varsel, numeric, same or fewer columns than Z, no missing
@@ -104,5 +85,4 @@ test_argsRemaining <- function() {
                'max(ztest) <= ncol(Z) is not TRUE', fixed=TRUE)
   expect_error(testfit<-kmbayes(y=y, X=X, Z=Z, varsel=TRUE, ztest=c(1,NA,2)), 
                'anyNA(ztest) == FALSE is not TRUE', fixed=TRUE)
-}
-
+})
