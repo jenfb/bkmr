24101eecaceb9a6bd1a7286295811f6f57b92fc6
Jennifer Bobb <bobb.j@ghc.org>
2016-06-08 09:57:45 -0700

    Change default prior specification

--- R/bkmr_main_functions.R
+++ R/bkmr_main_functions.R
@@ -202,10 +202,10 @@ kmbayes <- function(y, Z, X, iter = 1000, family = "gaussian", id, verbose = FAL
   
   ## control parameters
   if (!missing(control.params)){
-    control.params <- modifyList(list(lambda.jump = 10, mu.lambda = 10, sigma.lambda = 10, a.p0 = 1, b.p0 = 1, r.prior = "gamma", a.sigsq = 1e-3, b.sigsq = 1e-3, mu.r = 5, sigma.r = 5, r.muprop = 1, r.jump = 0.2, r.jump1 = 2, r.jump2 = 0.2, r.a = 0, r.b = 100), as.list(control.params))
+    control.params <- modifyList(list(lambda.jump = 10, mu.lambda = 10, sigma.lambda = 10, a.p0 = 1, b.p0 = 1, r.prior = "invunif", a.sigsq = 1e-3, b.sigsq = 1e-3, mu.r = 5, sigma.r = 5, r.muprop = 1, r.jump = 0.2, r.jump1 = 2, r.jump2 = 0.2, r.a = 0, r.b = 100), as.list(control.params))
     validateControlParams(varsel, family, id, control.params)
   } else {
-    control.params <- modifyList(list(lambda.jump = 10, mu.lambda = 10, sigma.lambda = 10, a.p0 = 1, b.p0 = 1, r.prior = "gamma", a.sigsq = 1e-3, b.sigsq = 1e-3, mu.r = 5, sigma.r = 5, r.muprop = 1, r.jump = 0.2, r.jump1 = 2, r.jump2 = 0.2, r.a = 0, r.b = 100), control.params)
+    control.params <- modifyList(list(lambda.jump = 10, mu.lambda = 10, sigma.lambda = 10, a.p0 = 1, b.p0 = 1, r.prior = "invunif", a.sigsq = 1e-3, b.sigsq = 1e-3, mu.r = 5, sigma.r = 5, r.muprop = 1, r.jump = 0.2, r.jump1 = 2, r.jump2 = 0.2, r.a = 0, r.b = 100), control.params)
   }
   
   control.params$r.params <- with(control.params, list(mu.r = mu.r, sigma.r = sigma.r, r.muprop = r.muprop, r.jump = r.jump, r.jump1 = r.jump1, r.jump2 = r.jump2, r.a = r.a, r.b = r.b))
