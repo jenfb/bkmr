c2705d8a31d32929e3f19d26d6d112c78dbcfa45
Jennifer F. Bobb <jenniferfederbobb@gmail.com>
2016-06-20 15:27:02 -0700

    Update package startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("For guided examples, go to 'https://jenfb.github.io/bkmr/bkmr.html'")
+  packageStartupMessage("For guided examples, go to 'https://jenfb.github.io/bkmr/overview.html'")
 }
