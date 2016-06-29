f781c63c0f72147078d1ecc834a2b66c3fb993ec
Jennifer F. Bobb <jenniferfederbobb@gmail.com>
2016-06-28 17:22:16 -0700

    Add release_questions function

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,9 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("For guided examples, go to 'https://jenfb.github.io/bkmr/overview.html'")
 }
+
+release_questions <- function() {
+  c(
+    "Have you updated the vignette and posted to GitHub?"
+  )
+}
