dc58e9e633fd6207085bba6aa1333bacd540edda
Jennifer F. Bobb <jenniferfederbobb@gmail.com>
2016-12-29 16:56:58 -0800

    Update to use quadratic function

--- scratch.R
+++ scratch.R
@@ -1,25 +1,43 @@
 devtools::load_all()
+library(magrittr)
 
 ## generate data ####
 
 #seed <- 1234
 #seed <- 8
-seed <- 31
+#seed <- 31
 set.seed(seed)
-datp <- SimData(hfun = 1, family = "binomial", beta.true = 0.1)
+n = 200
+M = 5
+sigsq.true = 1
+beta.true = 0.1
+Z <- matrix(runif(n * M, -1, 1), n, M)
+X <- cbind(3*cos(Z[, 1]) + 2*rnorm(n))
+eps <- rnorm(n, sd = sqrt(sigsq.true))
+#h <- 4*plogis(Z[, 1], 0, 0.3)
+h <- (2*Z[, 1] + 0.5) ^ 2
+plot(Z[, 1], h)
+
+mu <- X * beta.true + h
+y <- drop(mu + eps)
+  ystar <- y
+  y <- ifelse(ystar > 0, 1, 0)
+dat <- list(n = n, M = M, sigsq.true = sigsq.true, beta.true = beta.true, Z = Z, h = h, X = X, y = y)
+  dat$ystar <- ystar
+datp <- dat
+#datp <- SimData(hfun = 1, family = "binomial", beta.true = 0.1)
 y <- datp$y
 Z <- datp$Z
 X <- datp$X
 mu <- datp %$% drop(h + X*beta.true)
-y
 #str(datp)
 table(y)
 z1ord <- order(Z[, 1])
 
 plot(Z[, 1], datp$ystar)
 abline(lm(datp$h ~ Z[, 1]), col = "blue")
-lines(Z[z1ord, 1], predict(lm(datp$h ~ splines::ns(Z[, 1], 3)))[ord], col = "blue")
-lines(Z[z1ord, 1], datp$h[ord], col = "red", lwd = 2)
+lines(Z[z1ord, 1], predict(lm(datp$h ~ splines::ns(Z[, 1], 3)))[z1ord], col = "blue")
+lines(Z[z1ord, 1], datp$h[z1ord], col = "red", lwd = 2)
 
 ## linear probit model ####
 
@@ -35,11 +53,34 @@ abline(0, 1, col = "red")
 
 ## BKM probit model ####
 
-#fitpr <- kmbayes(iter = 10000, y = y, Z = Z, X = X, family = "binomial", varsel = TRUE, control.params = list(verbose_show_ests = TRUE, r.prior = "gamma"))
-#fitpr <- kmbayes(iter = 5000, y = y, Z = Z, X = X, family = "binomial", varsel = TRUE, control.params = list(verbose_show_ests = TRUE, r.prior = "invunif"))
+fitpr_gam0 <- kmbayes(iter = 5000, y = y, Z = Z, X = X, family = "binomial", varsel = TRUE, control.params = list(verbose_show_ests = TRUE, r.prior = "gamma"))
+fitpr <- fitpr_gam0
+
+fitpr_gam1 <- kmbayes(iter = 5000, y = y, Z = Z, X = X, family = "binomial", varsel = TRUE, control.params = list(verbose_show_ests = TRUE, r.prior = "gamma", r.jump2 = 1))
+fitpr <- fitpr_gam1
+
+fitpr_gam2 <- kmbayes(iter = 5000, y = y, Z = Z, X = X, family = "binomial", varsel = TRUE, control.params = list(verbose_show_ests = TRUE, r.prior = "gamma", r.jump2 = 2))
+fitpr <- fitpr_gam2
+
+fitpr_iu <- kmbayes(iter = 5000, y = y, Z = Z, X = X, family = "binomial", varsel = TRUE, control.params = list(verbose_show_ests = TRUE, r.prior = "invunif"))
+fitpr <- fitpr_iu
+
+fitpr_iu2 <- kmbayes(iter = 5000, y = y, Z = Z, X = X, family = "binomial", varsel = TRUE, control.params = list(verbose_show_ests = TRUE, r.prior = "invunif", r.jump1 = 3))
+fitpr <- fitpr_iu2
+
 sel <- with(fitpr, seq(floor(iter/2) + 1, iter))
 ests <- ExtractEsts(fitpr)
 #summary(fitpr)
+
+TracePlot(fit = fitpr, par = "beta")
+TracePlot(fit = fitpr, par = "sigsq.eps")
+TracePlot(fit = fitpr, par = "r", comp = 1)
+TracePlot(fit = fitpr, par = "r", comp = 2)
+TracePlot(fit = fitpr, par = "r", comp = 3)
+TracePlot(fit = fitpr, par = "r", comp = 4)
+TracePlot(fit = fitpr, par = "r", comp = 5)
+TracePlot(fit = fitpr, par = "h", comp = 20)
+TracePlot(fit = fitpr, par = "ystar", comp = 1)
 
 ## beta
 cbind(truth = datp$beta.true, ests$beta) %>% t()
