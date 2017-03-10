668c4213ea453ec2b24caf9869a85398004f2acc
Jennifer F. Bobb <jenniferfederbobb@gmail.com>
2017-03-09 17:12:23 -0800

    Update function to use approximate method for estimating the posterior mean of h at observed Z points

--- R/ComputePostmeanHnew.R
+++ R/ComputePostmeanHnew.R
@@ -3,9 +3,9 @@
 #' Function to approximate the posterior mean and variance as a function of the estimated tau, lambda, beta, and sigsq.eps
 #' @inheritParams kmbayes
 #' @inheritParams ExtractEsts
-#' @param sel A vector selecting which iterations of the BKMR fit should be retained for inference. Currently only implemented for \code{method == "r"}.
+#' @param sel A vector selecting which iterations of the BKMR fit should be retained for inference. 
 #' @export
-ComputePostmeanHnew <- function(fit, y = NULL, Z = NULL, X = NULL, Znew, sel = NULL) {
+ComputePostmeanHnew <- function(fit, y = NULL, Z = NULL, X = NULL, Znew = NULL, sel = NULL) {
   
   if (inherits(fit, "bkmrfit")) {
     if (is.null(y)) y <- fit$y
@@ -13,21 +13,30 @@ ComputePostmeanHnew <- function(fit, y = NULL, Z = NULL, X = NULL, Znew, sel = N
     if (is.null(X)) X <- fit$X
   }
   
+  if (!is.null(Znew)) {
     if(is.null(dim(Znew))) Znew <- matrix(Znew, nrow=1)
     if(class(Znew) == "data.frame") Znew <- data.matrix(Znew)
-    if(is.null(dim(X))) X <- matrix(X, ncol=1)
-
-    ests <- ExtractEsts(fit, sel = sel)
-    sigsq.eps <- ests$sigsq.eps[, "mean"]
-    r <- ests$r[, "mean"]
-    beta <- ests$beta[, "mean"]
-    lambda <- ests$lambda[, "mean"]
-    if (fit$family == "gaussian") {
-      ycont <- y
-    } else if (fit$family == "binomial") {
-      ycont <- ests$ystar[, "mean"]
-    }
-
+  }
+  if(is.null(dim(X))) X <- matrix(X, ncol=1)
+  
+  ests <- ExtractEsts(fit, sel = sel)
+  sigsq.eps <- ests$sigsq.eps[, "mean"]
+  r <- ests$r[, "mean"]
+  beta <- ests$beta[, "mean"]
+  lambda <- ests$lambda[, "mean"]
+  if (fit$family == "gaussian") {
+    ycont <- y
+  } else if (fit$family == "binomial") {
+    ycont <- ests$ystar[, "mean"]
+  }
+  
+  Kpart <- makeKpart(r, Z)
+  K <- exp(-Kpart)
+  V <- diag(1, nrow(Z), nrow(Z)) + lambda[1]*K
+  cholV <- chol(V)
+  Vinv <- chol2inv(cholV)
+  
+  if (!is.null(Znew)) {
     # if(is.null(data.comps$knots)) {
     n0 <- nrow(Z)
     n1 <- nrow(Znew)
@@ -37,19 +46,18 @@ ComputePostmeanHnew <- function(fit, y = NULL, Z = NULL, X = NULL, Znew, sel = N
     Kmat0 <- Kmat[1:n0,1:n0 ,drop=FALSE]
     Kmat1 <- Kmat[(n0+1):nall,(n0+1):nall ,drop=FALSE]
     Kmat10 <- Kmat[(n0+1):nall,1:n0 ,drop=FALSE]
-
-    Kpart <- makeKpart(r, Z)
-    V <- diag(1, nrow(Z), nrow(Z)) + lambda[1]*exp(-Kpart)
-    cholV <- chol(V)
-    Vinv <- chol2inv(cholV)
-
+    
     lamK10Vinv <- lambda[1]*Kmat10 %*% Vinv
-    Sigma.hnew <- lambda[1]*sigsq.eps*(Kmat1 - lamK10Vinv %*% t(Kmat10))
-    mu.hnew <- lamK10Vinv %*% (ycont - X%*%beta)
+    postvar <- lambda[1]*sigsq.eps*(Kmat1 - lamK10Vinv %*% t(Kmat10))
+    postmean <- lamK10Vinv %*% (ycont - X%*%beta)
     # } else {
     # stop("GPP not yet implemented")
     # }
-
-    ret <- list(postmean = drop(mu.hnew), postvar = Sigma.hnew)
-    ret
+  } else {
+    lamKVinv <- lambda[1]*K%*%Vinv
+    postvar <- lambda[1]*sigsq.eps*(K - lamKVinv%*%K)
+    postmean <- lamKVinv %*% (ycont - X%*%beta)
+  }
+  ret <- list(postmean = drop(postmean), postvar = postvar)
+  ret
 }
