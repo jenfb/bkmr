7dea631a3cd9f0c456df9625188f189b0a33975c
Jennifer F. Bobb <jenniferfederbobb@gmail.com>
2017-03-10 16:59:51 -0800

    Update documentation; add check that y is in (0, 1)

--- R/bkmr_main_functions.R
+++ R/bkmr_main_functions.R
@@ -67,7 +67,7 @@ makeVcomps <- function(r, lambda, Z, data.comps) {
 #' @param control.params list of parameters specifying the prior distributions and tuning parameters for the MCMC algorithm. If not specified default values will be chosen.
 #' @param varsel TRUE or FALSE: indicator for whether to conduct variable selection on the variables in \code{h}
 #' @param groups optional vector (of length \code{M}) of group indictors for fitting hierarchical variable selection if varsel=TRUE. If varsel=TRUE without group specification, component-wise variable selections will be performed.
-#' @param knots optional matrix of knot locations for implementing the Gaussian predictive process of Banerjee et al (2008). Currently only implemented for \code{family == gaussian} and models without a random intercept.
+#' @param knots optional matrix of knot locations for implementing the Gaussian predictive process of Banerjee et al (2008). Currently only implemented for models without a random intercept.
 #' @param ztest optional vector indicating on which variables in Z to conduct variable selection (the remaining variables will be forced into the model).
 #' @param rmethod for those predictors being forced into the \code{h} function, the method for sampling the \code{r[m]} values. Takes the value of 'varying' to allow separate \code{r[m]} for each predictor; 'equal' to force the same \code{r[m]} for each predictor; or 'fixed' to fix the \code{r[m]} to their starting values
 #' @param est.h TRUE or FALSE: indicator for whether to sample from the posterior distribution of the subject-specific effects h_i within the main sampler
@@ -100,6 +100,9 @@ kmbayes <- function(y, Z, X = NULL, iter = 1000, family = "gaussian", id = NULL,
   }
   if (family == "binomial") {
     message("Fitting probit regression model")
+    if (!all(y %in% c(0, 1))) {
+      stop("When family == 'binomial', y must be a vector containing only zeros and ones")
+    }
   }
   if (rmethod != "varying" & rmethod != "equal" & rmethod != "fixed") {
     message ("invalid value for rmethod, resetting to default varying")
